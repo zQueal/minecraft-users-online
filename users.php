2ab13770ec49f7da78665576fe6923364282227a
Zach Q <zach.queal@gmail.com>
2012-10-18 11:37:23 +0000

    Added better comments

--- users.php
+++ users.php
@@ -6,16 +6,16 @@ license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ */
 
 function numberOnline(){
 	global $file,$lines,$text,$pieces;
-        $file = "http://direct.wonderproject.net/uonline.txt";
+        $file = "http://direct.wonderproject.net/uonline.txt"; //Update to suit you
         $lines = count(file($file));
-        $text = "No one is online right now!|person|people";
+        $text = "No one is online right now!|person|people"; //No one online message and person(s) tense
         $pieces = explode('|', $text);
 
-        if($lines == 0){
+        if($lines == 0){ //If usercount == 0
                 echo $pieces[0]."<br /><br />";
-        } if($lines == 1){
+        } if($lines == 1){ //If usercount == 1
                 echo $lines." ".$pieces[1]." is online!<br /><br />";
-        } if($lines > 1){
+        } if($lines > 1){ //If usercount is more than 1
                 echo "There are ".$lines." ".$pieces[2]." are online!<br /><br />";
         }
 }
